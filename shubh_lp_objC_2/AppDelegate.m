4c98c925f24791966c190c9c8f18a33f6a5901ac
whatsinname <138867807+whatsin-name@users.noreply.github.com>
2023-09-07 16:23:35 +0530

    pn_fix_03

--- shubh_lp_objC_2/AppDelegate.m
+++ shubh_lp_objC_2/AppDelegate.m
@@ -39,9 +39,6 @@
     return YES;
 }
 
-//Note: You can choose any combination of formats — this is just an example.
-//NSString *registerForRemoteNotifications = @"Welcome to CT";
-
         - (void)applicationWillResignActive:(UIApplication *)application {
             // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
             // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
